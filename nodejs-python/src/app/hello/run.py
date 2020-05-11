e03634641564768bae37fc7e00d79a868257702f
Erivelton Facundo <eng.erivelton@gmail.com>
2020-05-10 23:16:23 -0300

    fix Dockerfile

--- nodejs-python/src/app/hello/run.py
+++ nodejs-python/src/app/hello/run.py
@@ -2,7 +2,7 @@ import ptvsd
 from . import handler
 
 ptvsd.enable_attach(address=('0.0.0.0', 5678), redirect_output=True)
-# ptvsd.wait_for_attach()
+ptvsd.wait_for_attach()
 
 
 def hello(event, context):
