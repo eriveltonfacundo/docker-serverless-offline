415562e50bc047b25f6231ed432a7beb83115b4a
Erivelton Facundo <eng.erivelton@gmail.com>
2020-05-10 23:08:25 -0300

    add request python

--- nodejs-python/src/app/hello/handler.py
+++ nodejs-python/src/app/hello/handler.py
@@ -1,12 +1,14 @@
 import uuid
 import json
+import requests
 
 
 def hello(event, context):
+    res = requests.get('https://scotch.io')
     return {
         "headers": {
             "Content-Type": "text/plan"
         },
-        "statusCode": 200,
+        "statusCode": res.status_code,
         "body": "olá python " + str(uuid.uuid4()),
     }
